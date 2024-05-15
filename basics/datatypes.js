3558cac6b43bf5f4048b06cc2de370af17a4550e
Abhay Singh <82321625+abhay121-pixel@users.noreply.github.com>
2024-05-15 20:19:24 +0000

    commit

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+" use strict"; //treat all js code as newer version
+
+// alert(3+3) this will not work bz we are using nodejs not browser
+
+let name="abhay"
+let age=23
+let loggedin=false
+
+//number=>2^53
+//bigint
+//string
+//boolean
+//null=>standalone value
+//undefined=>
+//symbol=> unique
+
+//object
+
+
+console.log(typeof 'abhay')
+
