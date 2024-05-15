3de8e1445a0ce6dc07eae15f03bb99d9bbb6cd0d
Abhay Singh <82321625+abhay121-pixel@users.noreply.github.com>
2024-05-15 20:02:34 +0000

    commit

---
+++ basics/1st.js
@@ -0,0 +1,17 @@
+const accountId=12121
+let accountEmail="Abhasyingh80446@gmail.com"
+var accountpassword="1234"
+accountcity="Lucknow"
+let accountstate;
+
+
+accountEmail="hysisnghgh"
+accountpassword='234235'
+accountcity='ayodhya'
+/*
+perfer not use var becuase of issue in block scope  and functional scope 
+*/
+
+console.log(accountId)
+
+console.table([accountEmail,accountId,accountpassword,accountstate,accountcity])
